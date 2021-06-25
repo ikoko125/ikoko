fb01b6518fafe037bf1a530df910356de0102644
ikoko125 <68625796+ikoko125@users.noreply.github.com>
2021-06-25 14:18:48 +0900

    test

--- pages/works/minecraft/index.vue
+++ pages/works/minecraft/index.vue
@@ -1,61 +1,69 @@
 <template>
     <section>
-        <div>
-            <h1>工事中です</h1>
+        <div id="mctop">
+            <div class="wrapper">
+            <h1>Minecraft</h1>
+            </div>
         </div>
-        <div>
-            <h2>Minecraftについて</h2>
-            <p>minecraftは2009年にcave gameとしてリリースされた同人ゲームでした。その後2011年末ごろまでβリリースなどが続けられ、製品版としてついにリリースされ人気も増していきました</p>
+        <h2>About</h2>
+        <div id="intro">
+            <p>Minecraftはレゴのようなブロックを使って様々なことをするゲームです。<a href="https://www.minecraft.net/en-us/partner">(一部の人はゲーム向けコンテンツ制作のため仕事をしている)</a></p>
+            <p>minecraftは2009年にcave gameとしてリリースされた同人ゲームでした。その後2011年末ごろまでβリリースなどが続けられ、その後まもなく製品版としてリリースされ人気も増していきました</p>
             <p>日本での一般層への普及は2014年半ば頃ですが、ネットの住人たちが目をつけていたのは2010年末～2013年頃のことです。</p>
-            <p>私は2012年小学6年生の頃にMinecraftを購入しました。当時のプレイヤー人口は500万人程度でした。(現在は2億人ほど)</p>
+            <p>私は2012年の春先、小学6年生の頃にMinecraftを購入しました。(当時のプレイヤー人口は500万人程)これがのちほどのほぼすべての活動のきっかけとなっています。ある意味人生を壊されたコンテンツです。</p>
+
         </div>
-    <h2>作品</h2>
-        <div id="work">
+    <h2>CATEGORY</h2>
+    <div class="catw">
+        <div class="cat">
+            <nuxt-link to="/works/minecraft/RTM/">
+            <div class = "sm smbg1">
+                <div class="desc">
+                    <p>RTM</p>
+                </div>
+            </div>
+            </nuxt-link>
+            <nuxt-link to="/works/minecraft/modern/">
+            <div class = "sm smbg2">
+                <div class="desc">
+                    <p>MODERN BUILD</p>
+                </div>
+            </div>
+            </nuxt-link>
+            <nuxt-link to="/works/minecraft/server/">
+                <div class = "sm smbg3">
+                    <div class="desc">
+                        <p>SERVER OPERATION</p>
+                    </div>
+                </div>
+            </nuxt-link>
+            <nuxt-link to="/works/minecraft/other/">
+            <div class = "sm smbg4">
+                <div class="desc">
+                    <p>OTHER BUILD</p>
+                </div>
+            </div>
+            </nuxt-link>
+        </div>
+    </div>
+    <!-- <h2>作品</h2>
+        <div class="cat">
             <div class = "bigview bg1"></div>
             <div class = "bigview bg2"></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
+            <div class="view"></div>
+            <div class="view"></div>
+            <div class="view"></div>
+            <div class="view"></div>
+            <div class="view"></div>
+            <div class="view"></div>
+            <div class="view"></div>
+            <div class="view"></div>
+            <div class="view"></div>
+        </div> -->
+    
     </section>
 </template>
 
 
-<style lang="scss" scoped>
-    section{
-        background-color: rgb(37, 18, 31);
-        #work{
-            width: 100%;
-            max-width: 1400px;
-            margin: 0 auto;
-            display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
-            div{
 
-            width: 240px;
-            height: 240px;
-            margin: 5px;
-            background-color: #f1f1f1;
-            }
-            .bigview{
-                width: 490px;
-                height: 240px;
-                margin: 5px;
-                background-color: #f1f1f1;
-                background-size: cover;
-
-            }
-            .bg1{
-                background-image: url(/image/minecraft/view1.png);
-            }
-        }
-    }
-</style>
+<style lang="scss" scoped src="./mctop.scss"></style>
--- pages/works/minecraft/server/index.vue
+++ pages/works/minecraft/server/index.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>工事中です</h1>
+    <section>
+        <h1>工事中です</h1>
+        <div id="sel">
+            <nuxt-link to="">
+                <div>
+                    <p>芦原サーバー(1.7.10)</p>
+                </div>
+            </nuxt-link>
+            <nuxt-link to="">
+                <div>
+                    <p>芦原サーバー(1.12.2)</p>
+                </div>
+            </nuxt-link>
+        </div>
+    </section>
 </template>
+
+<style lang="scss" scoped>
+    section{
+        #sel{
+            *{
+                text-decoration: none;
+            }
+            
+        }
+    }
+</style>
